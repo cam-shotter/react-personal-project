3af9869c601e29fbfeb6f9ecd328a7f03652a817
Cameron Shotter <cameronshotter@gmail.com>
2016-08-18 15:09:40 +1200

    pluged in a login form

--- components/login.js
+++ components/login.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Link, IndexLink } from 'react-router'
+import App from './app'
 import NavLink from './NavLink'
-import Home from './Home'
-import Images from './Images'
+
 
 class Login extends React.Component {
   render() {
     return (
-      <div>
-        <h1>React Router Tutorial</h1>
-        <ul role='nav'>
-          <li><NavLink to='/' onlyActiveOnIndex={true}>Home</NavLink></li>
-        </ul>
+      <div className="container">
+        <div className="login">
+          <h1 className='login-heading'>Login</h1>
+          <form method="post">
+            <input type="text" name="user" placeholder="Username" required="required" className="input-txt" />
+              <input type="password" name="password" placeholder="Password" required="required" className="input-txt" />
+              <div className="login-footer">
+                 <a href="#" className="lnk">
+                  <span className="icon icon-min">ಠ╭╮ಠ</span>
+                  I've forgotten something
+                </a>
+                <button type="submit" className="btn btn-right">Sign in  </button>
 
-        {this.props.children || <Home/>}
+              </div>
+          </form>
+        </div>
       </div>
     )
   }
--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 
+import Login from './components/login'
 import App from './components/app'
 import Menu from './components/menu'
 
+// <Route path='/login' component='{}' />
 
 render((
   <Router history={hashHistory}>
-    <Route path='/' component={App} >
-      <Route path='/menu' component={Menu} />
-
+    <Route path='/' component={Login} >
     </Route>
+    <Route path='/home' component={App} />
   </Router>
 ), document.querySelector('main'))
