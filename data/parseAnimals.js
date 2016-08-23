c085b967f2beb0b0e9a9d4211ef970518870d936
Cameron Shotter <cameronshotter@gmail.com>
2016-08-23 19:44:19 +1200

    messed around a lot, pretty sure not much has changed

--- data/parseAnimals.js
+++ data/parseAnimals.js
@@ -14,17 +14,27 @@ parseAnimals = (URLarray) => {
           let $ = cheerio.load(res.text)
 
           // The SPCA website has the titles of each thing eg: name, breed, adoption status under the same class name of "name". I'm creating an array to hold each "name" component, then mapping them to each "value" eg: [ 'Name: Fluffy', 'Breed: Short Hair Tabby' ]
-          let titlesArray = []
+          let titlesArray
           $('.name').map(() => {
-            titlesArray[0] = $('.name').text()
+            titlesArray = $('.name').text()
           })
 
-          let splitTitlesArray = titlesArray[0].split(': ')
+          // titlesArray returned in a very disorderly fashion so I have to use only the first element, then slice it from there.
+          let splitTitlesArray = titlesArray.split(': ')
+
+          let valuesArray
+          $('.value').each(() => {
+            valuesArray = $('.value').text()
+          })
+
+          // let splitValuesArray = valuesArray[0].split('')
 
           // The general class names don't relate to the content eg: '.pull-left' is the pet's name???? Whaaaa??? (Not that mine are much better)
           console.log("Name: ", $('.pull-left').text())
           console.log("TitleArray: ", titlesArray)
           console.log("I am the split title array: ", splitTitlesArray);
+          console.log("ValuesArray: ", valuesArray)
+
 
           // console.log("Description: ", $('.product-description p').text().substring(0, 200) + "...")
           // console.log("Image: ", $('.lightbox').html())
